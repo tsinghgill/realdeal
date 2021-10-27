1527302ee6f538942dd460c4baa6f83458f41c06
Dilraj <dsomel21@gmail.com>
2021-10-26 21:31:11 -0400

    Moved _getXYX to

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 require('dotenv').config();
 const kitchenerWaterlooV1 = require('./data/kitchenerWaterloo_v1');
 const { arrayToCsv } = require('./helpers/convert');
-
+const {
+  _getActiveHomesXPercentBelowAverageHomePrice,
+  _getAverageHomePrice,
+} = require('./helpers/formula');
 const puppeteer = require('puppeteer');
 const path = require('path');
 
@@ -152,57 +155,6 @@ async function scrapeResult(page, postalCode) {
     });
   }
   return scrappedResultsArr;
-}
-
-function _getAverageHomePrice(arrayOfObjects) {
-  let validHomesPricesInArr = 0;
-  let aggregatedHomePrices = 0;
-
-  arrayOfObjects.forEach(home => {
-    // Make sure the value is a number
-    // We increment validHomesPricesInArr so we divide and get the correct average home price in the area
-    if (typeof home.currentPrice === 'number' && home.status === 'CL') {
-      validHomesPricesInArr++;
-      aggregatedHomePrices = aggregatedHomePrices + home.currentPrice;
-    } else {
-      console.log(`Home price is either not a number or is not closed:`);
-      console.log(home);
-    }
-  });
-
-  return aggregatedHomePrices / validHomesPricesInArr;
-}
-
-function _getActiveHomesXPercentBelowAverageHomePrice(
-  discountPercentage,
-  averageHomePrice,
-  arrayOfHomes,
-  finalArray
-) {
-  let discountedHomes = [];
-
-  const maxHomePrice = averageHomePrice * (1 - discountPercentage);
-
-  arrayOfHomes.forEach(home => {
-    // console.log('✅ ✅ ✅ ✅ ✅ home ✅ ✅ ✅ ✅ ✅')
-    // console.log(home)
-    if (finalArray.some(finalHome => finalHome.mls === home.mls)) {
-      console.log('✅ ✅ ✅ ✅ ✅ duplicate home ✅ ✅ ✅ ✅ ✅');
-      // console.log(finalHome)
-    } else if (home.status === 'A' && home.currentPrice < maxHomePrice) {
-      home.averageHomePriceInTheArea = averageHomePrice; // Add a value to the home with average home price in the area
-      home.discountFromAverageHomePrice =
-        Math.round((1 - home.currentPrice / averageHomePrice) * 100) + '%'; // Add percentage discount
-      // console.log('✅ Found discounted home ✅')
-      // console.log(home)
-      discountedHomes.push(home);
-    }
-  });
-
-  console.log('✅ FINAL discountedHomes ✅');
-  console.log(discountedHomes);
-
-  return discountedHomes;
 }
 
 /**
