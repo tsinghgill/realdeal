13a68eac6574f46a03e2b5673198a0846152d6ef
Tanny <tanveet.gill@gmail.com>
2022-05-17 14:48:00 -0400

    getUniqueAddresses.js: Improved address extraction, filtered out irrelevant entries, and ensured uniqueness.

--- helpers/getUniqueAddresses.js
+++ helpers/getUniqueAddresses.js
@@ -1,58 +1,26 @@
-// This file works in conjunction with getUniqueAddressObjects
-// We get our address data from https://batch.openaddresses.io/data
-// First we use getUniqueAddressObjects.js to filter through the data from openaddresses
-// getUniqueAddressObjects.js gives us unique objects
-// Then we use getUniqueAddresses.js with the result of getUniqueAddressObjects.js
-// getUniqueAddresses.js gives us a list of addresses in strings as an array
-// This array is fed to the data folder which is then used by index.js to query for listings
+// This script processes address data, filters out irrelevant addresses, and formats them into a usable array.
+// It's designed to work with output from getUniqueAddressObjects.js, which provides unique address objects.
 
-// TODO: Clean this file up to import the data
-// TODO: Combine both getUniqueAddressObjects.js and getUniqueAddresses.js
-// TODO: The data from open addresses doesnt cover all the areas, we need to figure a way to cover every area
+const ADDRESSES = []; // Placeholder for address data
 
-const ADDRESSES = []
-
+/**
+ * Extracts unique addresses from the provided data.
+ * Filters out addresses with "Unit" or "Suite" in the street name and formats them for use in real estate queries.
+ */
 (async function getUniqueAddresses() {
     let uniqueAddresses = [];
 
-    // This will only happen once to populate the first item we compare with
+    // Loop through the address data and extract valid addresses
     ADDRESSES.forEach(addrObj => {
-        if (
-            addrObj.properties.number &&
-            addrObj.properties.street &&
-            !addrObj.properties.street.split(' ').includes('Unit') &&
-            !addrObj.properties.street.split(' ').includes('Suite')
-        ) {
-            uniqueAddresses.push(
-                `${addrObj.properties.number} ${addrObj.properties.street}, Hamilton, ON`
-            );
+        const { number, street, city } = addrObj.properties || {};
+
+        if (number && street && !street.split(' ').includes('Unit') && !street.split(' ').includes('Suite') && city) {
+            uniqueAddresses.push(`${number} ${street}, ${city}, ON`);
         }
     });
 
-    // console.log(uniqueAddresses)
-    console.log(JSON.stringify(uniqueAddresses, null, 1));
+    const uniqueAddressesArr = Array.from(new Set(uniqueAddresses)); // Ensure uniqueness of addresses
+
+    console.log('✅ Extracted Unique Addresses:');
+    console.log(JSON.stringify(uniqueAddressesArr, null, 2)); // Pretty print JSON
 })();
-
-// (async function getUniqueAddresses() {
-
-//     let uniqueAddresses = []
-
-//     // This will only happen once to populate the first item we compare with
-//     ADDRESSES.forEach(addrObj => {
-//         if (
-//             addrObj.properties.number &&
-//             addrObj.properties.street &&
-//             !addrObj.properties.street.split(" ").includes("Unit") &&
-//             !addrObj.properties.street.split(" ").includes("Suite") &&
-//             addrObj.properties.city &&
-//             (['Waterloo', 'Kitchener', 'Cambridge'].includes(addrObj.properties.city))
-//         ) {
-//             uniqueAddresses.push(
-//                 `${addrObj.properties.number} ${addrObj.properties.street}, ${addrObj.properties.city}`
-//             )
-//         }
-//     });
-
-//     // console.log(uniqueAddresses)
-//     console.log(JSON.stringify(uniqueAddresses, null, 1));
-// })();
